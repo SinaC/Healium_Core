727b956a2bdffa2bf9ef7f18f14a2872e7c2162e
Heymbeeck Joël <heymbeeck.joel@gmail.com>
2012-11-23 18:43:58 +0100

    3.0.3 continued
    Condition added on curse magic for shaman

--- Healium_Core/config/classes/shaman.lua
+++ Healium_Core/config/classes/shaman.lua
@@ -6,8 +6,7 @@ C["SHAMAN"] = {
 		[2] = { spellID = 1064 }, -- Chain Heal
 		[3] = { spellID = 974 }, -- Earth Shield
 		[4] = { spellID = 8004 }, -- Healing Surge
-		--[5] = { spellID = 51886, dispels = { ["Curse"] = true, ["Magic"] = function() return select(5, GetTalentInfo(3,12)) > 0 end } }, -- Cleanse Spirit
-		[5] = { spellID = 51886, dispels = { ["Curse"] = true } }, -- Cleanse Spirit
+		[5] = { spellID = 51886, dispels = { ["Curse"] = true, ["Magic"] = function() return GetSpecialization() == 3 and UnitLevel("player") >= 20 end } }, -- Cleanse Spirit or Purify Spirit
 		[6] = { spellID = 61295 }, -- Riptide
 		[7] = { spellID = 77472 },  -- Greater Healing Wave
 	},
